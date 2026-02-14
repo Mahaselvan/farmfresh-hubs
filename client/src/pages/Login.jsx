e2454bc98de6ff24e67ccf828d7618df56f81622
Mahaselvan <mahaselvanoffl@gmail.com>
2026-02-14 13:59:13 +0530

    login page updated

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -77,7 +77,7 @@ export default function Login() {
               </FormControl>
 
               <FormControl>
-                <FormLabel>Phone (optional)</FormLabel>
+                <FormLabel>Phone</FormLabel>
                 <Input value={phone} onChange={(e) => setPhone(e.target.value)} />
               </FormControl>
 
